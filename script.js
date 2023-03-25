3f7aec5e023b76709f0f78595077632478ccc0c6
ahmet <frontendmentoruser@gmail.com>
2023-03-26 00:55:35 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,60 @@
+const navA = document.querySelectorAll(".nav-link");
+
+
+
+var timeUp;
+var timeBack;
+
+for (let a of navA) {
+    // console.log(a);
+
+    if (window.innerWidth <= 1260) {
+
+
+
+        a.addEventListener("mouseenter", function (e) {
+
+            clearTimeout(timeBack);
+
+            timeUp = setTimeout(() => {
+
+                let span = e.target.children[1];
+                // console.log(e.target.children[1]);
+
+                span.classList.add("span-up");
+                span.classList.remove("span-back");
+
+            }, 1000);
+
+        });
+
+        a.addEventListener("mouseleave", function (e) {
+
+            clearTimeout(timeUp);
+
+            timeBack = setTimeout(() => {
+                let span = e.target.children[1];
+                // console.log(e.target.children[1]);
+                if (span.classList.contains("span-up")) {
+
+
+                    span.classList.add("span-back");
+                    span.classList.remove("span-up");
+                }
+
+            }, 100);
+
+        });
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
