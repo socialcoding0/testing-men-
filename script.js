c9489abbd33d6d76182bba8509de52c7fdb089c9
ahmet <frontendmentoruser@gmail.com>
2023-03-29 03:27:25 +0300

    update

--- script.js
+++ script.js
@@ -6,53 +6,55 @@ var timeUp;
 
 
 // menu event effect 
+window.addEventListener("DOMContentLoaded", function () {
+    for (let a of navA) {
 
-for (let a of navA) {
 
 
+        if (window.innerWidth <= 1260) {
 
-    if (window.innerWidth <= 1260) {
+            a.addEventListener("mouseenter", function (e) {
+                var timeBack;
 
-        a.addEventListener("mouseenter", function (e) {
-            var timeBack;
-
-            if (timeBack != undefined) {
-                clearTimeout(timeBack);
-                console.log(timeBack);
-            }
-
-            console.log(timeBack);
-
-            timeUp = setTimeout(() => {
-
-                let span = e.target.children[1];
-                // console.log(e.target.children[1]);
-
-                span.classList.add("span-up");
-                span.classList.remove("span-back");
-
-            }, 1000);
-
-        });
-
-        a.addEventListener("mouseleave", function (e) {
-
-            clearTimeout(timeUp);
-
-            timeBack = setTimeout(() => {
-                let span = e.target.children[1];
-                // console.log(e.target.children[1]);
-                if (span.classList.contains("span-up")) {
-                    span.classList.add("span-back");
-                    span.classList.remove("span-up");
+                if (timeBack != undefined) {
+                    clearTimeout(timeBack);
+                    console.log(timeBack);
                 }
 
-            }, 100);
+                console.log(timeBack);
 
-        });
+                timeUp = setTimeout(() => {
 
+                    let span = e.target.children[1];
+                    // console.log(e.target.children[1]);
+
+                    span.classList.add("span-up");
+                    span.classList.remove("span-back");
+
+                }, 1000);
+
+            });
+
+            a.addEventListener("mouseleave", function (e) {
+
+                clearTimeout(timeUp);
+
+                timeBack = setTimeout(() => {
+                    let span = e.target.children[1];
+                    // console.log(e.target.children[1]);
+                    if (span.classList.contains("span-up")) {
+                        span.classList.add("span-back");
+                        span.classList.remove("span-up");
+                    }
+
+                }, 100);
+
+            });
+
+        }
     }
-}
+});
+
 
 
 
